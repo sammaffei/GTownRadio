1969bce7e2926cffa6c181216d45ee2977d67988
Samuel Maffei <sammaffei@mac.com>
2018-09-07 15:27:21 -0400

    Starting check in.

---
+++ GTownRadio/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  gtownradio
+//
+//  Created by Samuel Maffei on 9/7/18.
+//  Copyright © 2018 Samuel Maffei. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ GTownRadio/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  gtownradio
+//
+//  Created by Samuel Maffei on 9/7/18.
+//  Copyright © 2018 Samuel Maffei. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
