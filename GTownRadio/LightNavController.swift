b41db16de3e9a4ea6aa8ca01120bafa6c3e5946e
Samuel Maffei <sammaffei@mac.com>
2018-10-28 17:09:08 -0400

    Update to light nav controller for UI.

---
+++ GTownRadio/LightNavController.swift
@@ -0,0 +1,17 @@
+//
+//  LightNavController.swift
+//  gtownradio
+//
+//  Created by Samuel Maffei on 10/28/18.
+//  Copyright © 2018 Samuel Maffei. All rights reserved.
+//
+
+import UIKit
+
+class LightNavController : UINavigationController
+    {
+    override var preferredStatusBarStyle: UIStatusBarStyle
+        {
+        return .lightContent
+        }
+    }
