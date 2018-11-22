75322df5e09e75f3f4ea88438a79828d8bfea3d0
Samuel Maffei <sammaffei@mac.com>
2018-11-21 19:32:37 -0500

    Moving more things to shared.

--- Shared/AppDelegate.swift
+++ Shared/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  gtowntv
+//  gtownradio
 //
-//  Created by Samuel Maffei on 11/21/18.
+//  Created by Samuel Maffei on 9/7/18.
 //  Copyright © 2018 Samuel Maffei. All rights reserved.
 //
 
@@ -16,12 +16,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FRadioPlayer.shared.isAutoPlay = false
+        
+        #if os(tvOS)
+        
+        FRadioPlayer.shared.radioURL = Constants.RadioPlayerURLs.HiFi
+        
+        #else
+        
+        FRadioPlayer.shared.radioURL = UserDefaults.standard.radioPlayerURL()
+        
+        #endif // set radio player url
+        
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
